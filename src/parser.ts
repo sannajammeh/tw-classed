379ae9aba078472ab0137bb791867869de202a4f
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-05-28 16:28:30 +0200

    feat: Add variant & defaultVariant support

--- src/parser.ts
+++ src/parser.ts
@@ -1,3 +1,42 @@
-import { ClassNames } from "./types.js";
+import {
+  ClassNames,
+  ClassNamesAndVariant,
+  VariantConfig,
+  Variants,
+} from "./types.js";
 
 export const classedParser = (classNames: ClassNames[]) => classNames.join(" ");
+
+export const composeParser = <T extends Variants>(
+  classNames: ClassNamesAndVariant<T>[]
+) => {
+  let stringClassNames = [];
+  let variantObj = {} as T;
+  let defaultVariants = {} as Partial<VariantConfig<T>["defaultVariants"]>;
+  for (const className of classNames) {
+    if (typeof className === "string") {
+      stringClassNames.push(className);
+      continue;
+    }
+
+    if (className.variants) {
+      variantObj = {
+        ...variantObj,
+        ...className.variants,
+      };
+    }
+
+    if (className.defaultVariants) {
+      defaultVariants = {
+        ...defaultVariants,
+        ...className.defaultVariants,
+      };
+    }
+  }
+
+  return {
+    className: classedParser(stringClassNames),
+    variants: variantObj,
+    defaultVariants,
+  };
+};
