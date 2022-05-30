799cd409ddde92b46fa2e48fca30867eca4c999a
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-05-30 06:05:32 +0200

    Improve monorepo & Add React Element classed support

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -11,23 +11,26 @@ import type {
   VariantProps,
 } from "./types.js";
 
-function classed<T extends keyof JSX.IntrinsicElements, V extends Variants>(
-  elementType: T,
-  ...classNames: ClassNamesAndVariant<V>[]
-) {
-  // const className = classedParser(classNames);
-
+function classed<
+  T extends keyof JSX.IntrinsicElements | React.ComponentType<any>,
+  V extends Variants
+>(elementType: T, ...classNames: ClassNamesAndVariant<V>[]) {
   const { className, variants, defaultVariants } = composeParser(classNames);
 
   const ClassedComponent = forwardRef(
     ({ as, className: cName, ...props }: any, forwardedRef: any) => {
-      const Component = as || elementType;
+      const Component = (elementType as any).__CLASSED_COMPONENT__
+        ? elementType
+        : as || elementType;
 
       // Map props variant to className
       const variantClassNames = useMemo(() => {
         return Object.keys(variants).reduce((acc, value) => {
           const vInProps = props[value] || defaultVariants?.[value]; // Prefer props over defaultVariants
           if (!vInProps) return acc; // Skip if no variant in props
+          if (props[value]) {
+            delete props[value];
+          }
           const className = variants[value][vInProps as string]; // Get className from variant
           return acc.concat(" " + className); // Add className to acc
         }, "");
@@ -41,6 +44,7 @@ function classed<T extends keyof JSX.IntrinsicElements, V extends Variants>(
             (variantClassNames ? " " + variantClassNames : "")
           }
           {...props}
+          as={(elementType as any).__CLASSED_COMPONENT__ ? as : undefined}
           ref={forwardedRef}
         />
       );
@@ -48,6 +52,8 @@ function classed<T extends keyof JSX.IntrinsicElements, V extends Variants>(
   ) as Polymorphic.ForwardRefComponent<T, VariantProps<V>>;
 
   ClassedComponent.displayName = `TwComponent(${elementType})`;
+
+  (ClassedComponent as any).__CLASSED_COMPONENT__ = true;
 
   return ClassedComponent;
 }
