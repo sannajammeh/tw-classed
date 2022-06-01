c51baee965751bfcbb066a97cac07fdfd3a94926
Sanna Jammeh <sannajam@outlook.com>
2022-06-01 23:04:52 +0200

    Setup tailwind

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,40 +1,7 @@
 import { NextPage } from "next";
-import classed from "tw-classed";
-
-const Title = classed("h1", "text-lg", {
-  variants: {
-    color: {
-      blue: "text-blue-500",
-    },
-    margin: {
-      sm: "mt-4",
-    },
-  },
-});
-
-const StyledComp = classed(Title, {
-  variants: {
-    size: {
-      large: "text-2xl",
-    },
-    color: {
-      red: "text-red-500",
-    },
-  },
-});
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Title>Hello World</Title>
-      <Title as="h2" color="blue">
-        Blue title
-      </Title>
-      <StyledComp as="a" href="http://localhost:3000" color="red" size="large">
-        sdsdsd
-      </StyledComp>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
