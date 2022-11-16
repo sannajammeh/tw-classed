46911aaa58274272bede7ee7bf0b5bb58bd96381
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-11-16 02:19:55 +0100

    docs: fix repo base

--- apps/beta-docs/theme.config.js
+++ apps/beta-docs/theme.config.js
@@ -5,7 +5,8 @@ import { TbBrandTailwind } from "react-icons/tb";
  * @type {import('nextra-theme-docs').DocsThemeConfig}
  */
 export default {
-  docsRepositoryBase: "https://github.com/sannajammeh/tw-classed/tree/master", // base URL for the docs repository
+  docsRepositoryBase:
+    "https://github.com/sannajammeh/tw-classed/tree/master/apps/beta-docs", // base URL for the docs repository
   titleSuffix: " - TwClassed",
   darkMode: true,
   project: {
