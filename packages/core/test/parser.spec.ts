9bd9261ac57955260942a19bf0b35a54ab9df630
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-11-18 22:35:53 +0100

    fix: write tests

--- packages/core/test/parser.spec.ts
+++ packages/core/test/parser.spec.ts
@@ -1,4 +1,8 @@
-import { parseClassNames } from "../src/parser";
+import {
+  getVariantSelector,
+  mapPropsToVariantClass,
+  parseClassNames,
+} from "../src/parser";
 import { describe, expect, it } from "vitest";
 
 describe("Classed Parser", () => {
@@ -12,6 +16,40 @@ describe("Classed Parser", () => {
     const result = parseClassNames(["test", "test 2"]);
 
     expect(result.className).toBe("test test 2");
+  });
+
+  it("Should parse variants", () => {
+    const result = parseClassNames([
+      "test",
+      {
+        variants: {
+          variant: {
+            test: "test",
+          },
+        },
+      },
+    ]);
+
+    expect(result.variants).toEqual({
+      variant: {
+        test: "test",
+      },
+    });
+  });
+
+  it("Should parse variant config className prop", () => {
+    const result = parseClassNames([
+      {
+        className: "test",
+        variants: {
+          variant: {
+            test: "test",
+          },
+        },
+      },
+    ]);
+
+    expect(result.className).toBe("test");
   });
 
   it("Should merge default variants", () => {
@@ -29,5 +67,73 @@ describe("Classed Parser", () => {
     ]);
 
     expect(result.defaultVariants.size).toBe("small");
+  });
+
+  it("Should merge function variants", () => {
+    const fn = () => "test";
+    fn._def = {
+      className: "test",
+      variants: {
+        size: {
+          large: "large",
+        },
+      },
+      defaultVariants: {
+        size: "large",
+      },
+    };
+
+    const result = parseClassNames([fn]);
+
+    expect(result.variants.size.large).toBe("large");
+    expect(result.defaultVariants.size).toBe("large");
+    expect(result.className).toBe("test");
+  });
+});
+
+describe("mapPropsToVariantClass()", () => {
+  it("Should map props correctly", () => {
+    const config = {
+      variants: {
+        size: {
+          large: "large",
+          small: "small",
+        },
+      },
+      defaultVariants: {
+        size: "large",
+      },
+    };
+    const result = mapPropsToVariantClass(config as any);
+
+    expect(result).toBe("large");
+
+    const result2 = mapPropsToVariantClass(config as any, { size: "small" });
+
+    expect(result2).toBe("small");
+  });
+
+  it("Should handle undefined variant selector", () => {
+    const config = {
+      variants: {
+        size: {
+          large: "large",
+          small: "small",
+        },
+      },
+    };
+    const result = mapPropsToVariantClass(config as any, { size: "test" });
+    expect(result).toBe("");
+
+    const result2 = mapPropsToVariantClass(config as any, {});
+    expect(result2).toBe("");
+
+    const fn = new Function();
+
+    fn.prototype.useMe = () => "test";
+
+    const result3 = mapPropsToVariantClass(config as any, fn as any);
+
+    expect(result3).toBe("");
   });
 });
