e2699965cd966822552652523f414f8fbe87b30f
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-11-19 01:56:43 +0100

    fix(core): switch to global symbol

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,4 +1,4 @@
 /**
  * Unique symbol used to reference the cummulitive variants,defaults & classNames of a Classed Component.
  */
-export const TW_VARS = Symbol("tw-vars");
+export const TW_VARS = Symbol.for("tw-classed.vars");
