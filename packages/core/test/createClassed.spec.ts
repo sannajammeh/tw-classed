57f3eb9f0be6af13637b7186fd2c2e201c5149fd
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-12-06 15:46:52 +0100

    test(core): import expect

--- packages/core/test/createClassed.spec.ts
+++ packages/core/test/createClassed.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { createClassed } from "../src";
 
 const stringIncludes = (str: string, str2: string) => {
