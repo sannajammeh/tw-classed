7ceaa00dae0c4d9cf5740ff3ef266699c8722101
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-12-07 20:38:28 +0100

    test(react): setup typechecking for test files

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -3,17 +3,10 @@
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
+  plugins: [react()],
   build: {
     lib: {
       entry: ["./src/index.tsx"],
@@ -36,5 +29,8 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./test/setup.ts",
+    typecheck: {
+      include: ["./test/**/*.{ts,tsx}"],
+    },
   },
 });
