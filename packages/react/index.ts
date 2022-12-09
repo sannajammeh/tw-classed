35f8a946effb3b863239649892502271e49ec202
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-12-09 21:15:57 +0100

    fix(react): export ClassedConfig type

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,6 +1,6 @@
 export { classedProxy as classed } from "./src/proxy";
 export { createClassed } from "./src/classed";
-
+export type { ClassedConfig } from "./src/classed";
 export type {
   InferVariantProps,
   ClassedComponentType,
