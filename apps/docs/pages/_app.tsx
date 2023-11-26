4121daaa562189605503175c1cbeacaa34d3d430
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2023-11-26 13:25:09 +0100

    add vercel analytics

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -2,7 +2,13 @@ import "styles/globals.css";
 import "nextra-theme-docs/style.css";
 import "styles/theme-override.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Nextra({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
